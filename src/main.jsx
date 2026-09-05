3f9e45ff1d0de0838cd1ed800b738664613d65d0
tinkamarket480-creator <tinkamarket480@gmail.com>
2026-09-04 21:35:57 -0500

    actualizacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import AdminPanel from './adminPanel.jsx'
 
-const esAdmin = window.location.hash === 'admin'
+const esAdmin = window.location.hash === '#admin'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {esAdmin ? <AdminPanel /> : <App />}
+    {esAdmin ? (
+      <AdminPanel />
+    ) : (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )}
   </StrictMode>,
 )
